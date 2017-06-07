8572db97a701533fd66160ad509690fb527bb032
Nick Humrich <nick.humrich@gmail.com>
2017-06-07 15:10:25 -0600

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dunderdoc',
-    version='0.1.0',
+    version='0.2.0',
     requires=['click'],
     packages=['dunderdoc'],
     url='https://github.com/nhumrich/dunderdoc',
